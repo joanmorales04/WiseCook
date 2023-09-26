c0fed602dd15e44904fd8a125927c20d800f24ba
Aj Nicolas <ajnicolas@Ajs-MacBook-Pr0.local>
2023-09-26 00:15:46 -0500

    connected server to client successfully

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [prepTime, setPrepTime] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
+  const [recipe, setRecipe] = useState(''); // Define recipe state variable
+
+  const handleIngredientChange = (event, index) => {
+    const newIngredients = [...ingredients];
+    newIngredients[index] = event.target.value;
+    setIngredients(newIngredients);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    // Constructing the payload to be sent to the server
+    const payload = {
+      prepTime,
+      ingredients,
+    };
+    
+    try {
+      // Making a POST request to the updated '/recipe' endpoint
+      const response = await fetch('http://localhost:8080/recipe', { // Updated the URL to '/recipe'
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+      
+      if (response.ok) {
+        const receivedRecipe = await response.json();
+        setRecipe(receivedRecipe.recipe);
+        setSubmitted(true);
+      } else {
+        console.error('Failed to submit form:', await response.text());
+      }
+    } catch (error) {
+      console.error('An error occurred while submitting the form:', error);
+    }
+  }
+
   return (
-    <div>
-      <h1> Hello </h1>
+    <div className="app-container">
+      <h1>WiseCook</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Select Prep Time:
+          <select value={prepTime} onChange={(e) => setPrepTime(e.target.value)}>
+            <option value="">Select</option>
+            <option value="15">15 mins</option>
+            <option value="30">30 mins</option>
+            <option value="60">1 hr</option>
+            <option value="over">over 1 hr</option>
+          </select>
+        </label>
+        
+        {ingredients.map((ingredient, index) => (
+          <input
+            key={index}
+            type="text"
+            placeholder="Enter ingredient"
+            value={ingredient}
+            onChange={(e) => handleIngredientChange(e, index)}
+          />
+        ))}
+        
+        <button type="button" onClick={() => setIngredients([...ingredients, ''])}>
+          Add Ingredient
+        </button>
+        
+        <button type="submit">Submit</button>
+      </form>
+      
+      {submitted && recipe && (
+        <div className="recipe-container">
+          <h2>Generated Recipe:</h2>
+          <p>{recipe}</p>
+          {/* Render other parts of the recipe as needed */}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,39 @@
-// connect to database
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+// const recipeRoutes = require('./routes/recipeRoutes.js');
+const recipeController = require('./controllers/recipeController');
+
+PORT=8080;
+
+// Serve the React app
+const app = express();
+app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST'],
+  credentials: true,
+}));
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.json());
+
+
+// Handle other routes or API endpoints here
+app.post('/recipe', async (req, res) => {
+
+	// grab ingredients and time from post request
+	const { prepTime, ingredients } = req.body;
+
+	// use prompt engineering to generate the recipe using the ingredients and time
+	const generatedPrompt = await recipeController.generatePrompt(prepTime, ingredients);
+
+	// after getting the prompt, call openai api to send api request to get recipe. 
+	const generatedRecipe = await recipeController.generateRecipe(generatedPrompt);
+
+	// return chatgpt response to front
+	res.json({ recipe: generatedRecipe });
+});
 
 
 
-// handle routes such as GET/POST
-
-// this is where the prompt function will be
-
-// this is where the api function will be too
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
